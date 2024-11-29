bc9895140511487c4049e1964e1b872519f43281
CaliNux <132522491+calinux-py@users.noreply.github.com>
2024-11-28 18:03:20 -0800

    Update whodat.py

--- whodat.py
+++ whodat.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QFont
 from gui import MainWindow
 
 if __name__ == "__main__":
@@ -10,4 +11,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
+    font = QFont()
+    font.setPointSize(9)
+    app.setFont(font)
     sys.exit(app.exec())
